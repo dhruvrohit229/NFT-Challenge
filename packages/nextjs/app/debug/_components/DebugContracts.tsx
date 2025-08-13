cf2919caa564a6811abf9d54ef8e006f9764cf7a
dhruvrohit229 <rohitdhruv205@gmail.com>
2025-08-13 10:19:45 +0000

    NFT challenge initialized

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { IStylusNFT } from "./IStylusNFT";
 import { ethers } from "ethers";
 
-const contractAddress = "0xe2a8cd01354ecc63a8341a849e9b89f14ff9f08f"; // Get this from run-dev-node.sh output
+const contractAddress="0x2f389e0d223e9b45b63341bd461e40ac45f05d04"; // Get this from run-dev-node.sh output
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
